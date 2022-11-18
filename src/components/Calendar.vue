b5c73f1c7eca1e751c4355f8e144802c5e6d0afb
keitarosuzuki <1548suzuki@gmail.com>
2022-11-19 02:46:37 +0900

    モーダル改修

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,143 +1,143 @@
 <template>
-  <div class="content">
-    <h2>カレンダー{{ displayDate }}</h2>
-    <div class="button-area">
-      <button @click="prevMonth" class="button">前の月</button>
-      <button @click="nextMonth" class="button">次の月</button>
-    </div>
-    <div class="calendar">
-      <div class="calendar-weekly">
-        <div class="calendar-youbi" v-for="n in 7" :key="n">
-          {{ youbi(n-1) }}
+    <div class="content">
+        <h2>{{ displayDate }}</h2>
+        <div class="button-area">
+            <button @click="prevMonth" class="button">前の月</button>
+            <button @click="nextMonth" class="button">次の月</button>
         </div>
-      </div>
-      <div 
-        class="calendar-weekly"
-        v-for="(week, index) in calendars"
-        :key="index"
-      >
-        <div
-          class="calendar-daily"
-          :class="{outside: currentMonth !== day.month}"
-          v-for="(day, index) in week"
-          :key="index"
-          data-bs-toggle="modal"
-          data-bs-target="#exampleModal"
-        >
-          <div class="calendar-day">
-            {{ day.day }}
-          </div>
+        <div class="calendar">
+            <div class="calendar-weekly">
+                <div class="calendar-youbi" v-for="n in 7" :key="n">
+                    {{ youbi(n - 1) }}
+                </div>
+            </div>
+            <div
+                class="calendar-weekly"
+                v-for="(week, index) in calendars"
+                :key="index"
+            >
+                <div
+                    class="calendar-daily"
+                    :class="{ outside: currentMonth !== day.month }"
+                    v-for="(day, index) in week"
+                    :key="index"
+                    data-bs-toggle="modal"
+                    data-bs-target="#staticBackdrop"
+                >
+                    <div class="calendar-day">
+                        {{ day.day }}
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </template>
 <script>
 import moment from "moment";
 export default {
-  data() {
-    return {
-      currentDate: moment(),
-    };
-  },
-  methods: {
-    getStartDate() {
-      let date = moment(this.currentDate);
-      date.startOf("month");
-      const youbiNum = date.day();
-      return date.subtract(youbiNum, "days");
+    data() {
+        return {
+            currentDate: moment(),
+        };
     },
-    getEndDate() {
-      let date = moment(this.currentDate);
-      date.endOf("month");
-      const youbiNum = date.day();
-      return date.add(6 - youbiNum, "days");
-    },
-    getCalendar() {
-      let startDate = this.getStartDate();
-      const endDate = this.getEndDate();
-      const weekNumber = Math.ceil(endDate.diff(startDate, "days") / 7);
+    methods: {
+        getStartDate() {
+            let date = moment(this.currentDate);
+            date.startOf("month");
+            const youbiNum = date.day();
+            return date.subtract(youbiNum, "days");
+        },
+        getEndDate() {
+            let date = moment(this.currentDate);
+            date.endOf("month");
+            const youbiNum = date.day();
+            return date.add(6 - youbiNum, "days");
+        },
+        getCalendar() {
+            let startDate = this.getStartDate();
+            const endDate = this.getEndDate();
+            const weekNumber = Math.ceil(endDate.diff(startDate, "days") / 7);
 
-      let calendars = [];
-      let calendarDate = this.getStartDate();
+            let calendars = [];
+            let calendarDate = this.getStartDate();
 
-      for (let week = 0; week < weekNumber; week++) {
-        let weekRow = [];
-        for (let day = 0;  day < 7; day++) {
-          weekRow.push({
-            day: calendarDate.get("date"),
-            month: calendarDate.format("YYYY-MM"),//追加
-          });
-          calendarDate.add(1, "days");
-        }
-        calendars.push(weekRow);
-      }
-      return calendars;
+            for (let week = 0; week < weekNumber; week++) {
+                let weekRow = [];
+                for (let day = 0; day < 7; day++) {
+                    weekRow.push({
+                        day: calendarDate.get("date"),
+                        month: calendarDate.format("YYYY-MM"), //追加
+                    });
+                    calendarDate.add(1, "days");
+                }
+                calendars.push(weekRow);
+            }
+            return calendars;
+        },
+        nextMonth() {
+            this.currentDate = moment(this.currentDate).add(1, "month");
+        },
+        prevMonth() {
+            this.currentDate = moment(this.currentDate).subtract(1, "month");
+        },
+        youbi(dayIndex) {
+            const week = ["日", "月", "火", "水", "木", "金", "土"];
+            return week[dayIndex];
+        },
     },
-    nextMonth() {
-      this.currentDate = moment(this.currentDate).add(1, "month");
+    computed: {
+        calendars() {
+            return this.getCalendar();
+        },
+        displayDate() {
+            return this.currentDate.format("YYYY[年]M[月]");
+        },
+        currentMonth() {
+            return this.currentDate.format("YYYY-MM");
+        },
     },
-    prevMonth() {
-      this.currentDate = moment(this.currentDate).subtract(1, "month");
-    },
-    youbi(dayIndex) {
-      const week = ["日", "月", "火", "水", "木", "金", "土"];
-      return week[dayIndex];
-    },
-  },
-  computed: {
-    calendars() {
-      return this.getCalendar();
-    },
-    displayDate(){
-      return this.currentDate.format('YYYY[年]M[月]')
-    },
-    currentMonth(){
-      return this.currentDate.format('YYYY-MM')
-    },
-  },
 };
 </script>
 
 <style>
-.content{
-  margin:2em auto;
-  width:900px;
+.content {
+    margin: 2em auto;
+    width: 900px;
 }
-.button-area{
-  margin:0.5em 0;
+.button-area {
+    margin: 0.5em 0;
 }
-.button{
-  padding:4px 8px;
-  margin-right:8px;
+.button {
+    padding: 4px 8px;
+    margin-right: 8px;
 }
-.calendar{
-  max-width:900px;
-  border-top:1px solid #E0E0E0;
-  font-size:0.8em;
+.calendar {
+    max-width: 900px;
+    border-top: 1px solid #e0e0e0;
+    font-size: 0.8em;
 }
-.calendar-weekly{
-  display:flex;
-  border-left:1px solid #E0E0E0;
-  /* background-color: black; */
+.calendar-weekly {
+    display: flex;
+    border-left: 1px solid #e0e0e0;
+    /* background-color: black; */
 }
-.calendar-daily{
-  flex:1;
-  min-height:125px;
-  border-right:1px solid #E0E0E0;
-  border-bottom:1px solid #E0E0E0;
-  margin-right:-1px;
+.calendar-daily {
+    flex: 1;
+    min-height: 125px;
+    border-right: 1px solid #e0e0e0;
+    border-bottom: 1px solid #e0e0e0;
+    margin-right: -1px;
 }
-.calendar-day{
-  text-align: center;
+.calendar-day {
+    text-align: center;
 }
-.calendar-youbi{
-  flex:1;
-  border-right:1px solid #E0E0E0;
-  margin-right:-1px;
-  text-align:center;
+.calendar-youbi {
+    flex: 1;
+    border-right: 1px solid #e0e0e0;
+    margin-right: -1px;
+    text-align: center;
 }
-.outside{
-  background-color: #f7f7f7;
+.outside {
+    background-color: #f7f7f7;
 }
 </style>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,21 +1,149 @@
 <template>
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div
+        class="modal fade"
+        id="staticBackdrop"
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+        tabindex="-1"
+        aria-labelledby="staticBackdropLabel"
+        aria-hidden="true"
+    >
         <div class="modal-dialog">
             <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                ...
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-success">収入</button>
-                <button type="button" class="btn btn-primary">支出</button>
-                <button type="button" class="btn btn-danger">削除</button>
-                <button type="button" class="btn btn-warning">修正</button>
-            </div>
+                <div class="modal-header">
+                    <h5
+                        v-if="flagTitle === ''"
+                        class="modal-title"
+                        id="exampleModalLabel"
+                    >
+                        どっち
+                    </h5>
+                    <h5
+                        v-if="flagTitle === 'in'"
+                        class="modal-title"
+                        id="exampleModalLabel"
+                    >
+                        収入 立て替えあった？
+                    </h5>
+                    <h5
+                        v-if="flagTitle === 'ex'"
+                        class="modal-title"
+                        id="exampleModalLabel"
+                    >
+                        出費
+                    </h5>
+                    <button
+                        type="button"
+                        class="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                        @click="clearModalVal"
+                    ></button>
+                </div>
+                <div class="modal-body">
+                    <form
+                        v-if="flagTitle === ''"
+                        class="form-floating"
+                        autocomplete="off"
+                    >
+                        <input
+                            type="text"
+                            class="form-control"
+                            :class="{
+                                'border border-danger border-5 rounded-3':
+                                    flagCheckInput === true,
+                            }"
+                            id="floatingInputValue"
+                            placeholder="金額を入力"
+                            v-model="valInput"
+                        />
+                        <label for="floatingInputValue">金額を入力</label>
+                    </form>
+                    <div v-else-if="flagTitle === 'in'">
+                        <h2>¥ {{ textPrice }}</h2>
+                        <div
+                            v-for="(title, index) in aryAdvances"
+                            :key="index"
+                            class="form-check"
+                        >
+                            <input
+                                class="form-check-input"
+                                type="checkbox"
+                                value=""
+                                id="flexCheckDefault"
+                            />
+                            <label
+                                class="form-check-label"
+                                for="flexCheckDefault"
+                            >
+                                {{ title }}
+                            </label>
+                        </div>
+                    </div>
+                    <div v-else-if="flagTitle === 'ex'">
+                        <h2>¥ {{ textPrice }}</h2>
+                        <form
+                            class="form-floating"
+                            autocomplete="off"
+                            v-for="(title, index) in aryAccountTitle"
+                            :key="index"
+                        >
+                            <input
+                                type="text"
+                                class="form-control"
+                                id="floatingInputValue"
+                                placeholder="{{title}}"
+                            />
+                            <label for="floatingInputValue">{{ title }}</label>
+                        </form>
+                        <div class="input-group">
+                            <div class="form-floating">
+                                <input
+                                    type="text"
+                                    class="form-control hoge"
+                                    id="floatingInputValue1"
+                                    placeholder="立替費"
+                                />
+                                <label for="floatingInputValue1">立替費</label>
+                            </div>
+                            <div class="form-floating">
+                                <input
+                                    type="text"
+                                    class="form-control hoge"
+                                    id="floatingInputValue2"
+                                    placeholder="なんの"
+                                />
+                                <label for="floatingInputValue2">なんの</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button
+                        type="button"
+                        class="btn btn-secondary"
+                        data-bs-dismiss="modal"
+                        @click="clearModalVal"
+                    >
+                        Close
+                    </button>
+                    <button
+                        type="button"
+                        class="btn btn-success"
+                        @click="registerIncomeVal"
+                    >
+                        収入
+                    </button>
+                    <button
+                        type="button"
+                        class="btn btn-warning"
+                        @click="registerExpenceVal"
+                    >
+                        出費
+                    </button>
+                    <!-- <button type="button" class="btn btn-danger">削除</button>
+                    <button type="button" class="btn btn-warning">修正</button> -->
+                </div>
             </div>
         </div>
     </div>
@@ -24,6 +152,67 @@
 <script>
 /* eslint-disable */
 export default {
-    name: 'Modal'
-}
+    name: "Modal",
+    data() {
+        return {
+            valInput: "",
+            aryAccountTitle: [
+                "食費",
+                "日用品",
+                "通信費",
+                "水道光熱費",
+                "医療費",
+                "保険料",
+                "家賃",
+                "交通費",
+                "その他出費",
+            ],
+            flagTitle: "",
+            aryAdvances: [
+                // 例
+                "交通費",
+                "スタジオ代",
+            ],
+            flagCheckInput: false,
+        };
+    },
+    methods: {
+        clearModalVal() {
+            this.flagTitle = "";
+            this.valInput = "";
+            this.flagCheckInput = false;
+        },
+        registerIncomeVal() {
+            if (this.checkValInput(this.valInput)) {
+                return false;
+            }
+            this.flagTitle = "in";
+        },
+        registerExpenceVal() {
+            if (this.checkValInput(this.valInput)) {
+                return false;
+            }
+            this.flagTitle = "ex";
+        },
+        checkValInput(input) {
+            var regexp = new RegExp(/^[0-9]+(\.[0-9]+)?$/);
+            if (!regexp.test(input)) {
+                this.flagCheckInput = true;
+                return true;
+            }
+        },
+    },
+    computed: {
+        textPrice() {
+            return this.valInput.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+        },
+    },
+    created() {},
+};
 </script>
+
+<style scoped>
+.hoge {
+    padding-right: 24.5px;
+}
+</style>
